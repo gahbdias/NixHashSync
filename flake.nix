47eb3c95f9912d707fa10a48643cb40986e75f8c
luis <luis@pleme.io>
2024-10-14 17:24:35 -0300

    bufferline nord

--- flake.nix
+++ flake.nix
@@ -4,32 +4,33 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     poetry2nix.url = "github:nix-community/poetry2nix";
+    flake-utils.url = github:numtide/flake-utils?branch=master;
   };
 
-  outputs = { self, nixpkgs, poetry2nix }:
-    let
-      system = "x86_64-darwin";
-      overlays = import ./overlays;
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
-      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
-      myPythonApp = mkPoetryApplication { projectDir = ./.; };
-    in
-    {
-      apps.${system}.default = {
-        type = "app";
-        program = "${myPythonApp}/bin/nixhashsync";
-      };
+  outputs = { self, nixpkgs, poetry2nix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = import ./overlays;
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+        myPythonApp = mkPoetryApplication { projectDir = ./.; };
+      in
+      {
+        apps.${system}.default = {
+          type = "app";
+          program = "${myPythonApp}/bin/nixhashsync";
+        };
 
-      packages.${system}.default = myPythonApp;
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          myPythonApp
-          pkgs.direnv
-          pkgs.poetry
-        ];
-      };
-    };
+        packages.${system}.default = myPythonApp;
+        devShells.${system}.default = pkgs.mkShell {
+          buildInputs = [
+            myPythonApp
+            pkgs.direnv
+            pkgs.poetry
+          ];
+        };
+      });
 }
 
