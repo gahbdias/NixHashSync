fad8e0fb10e18878608d7bb2cb818d21069ae08d
luis <ldesiqueira@pinger.com>
2024-10-14 13:47:18 -0300

    reduce flake.nix

--- flake.nix
+++ flake.nix
@@ -2,37 +2,13 @@
   description = "My NixHashSync application with Poetry integration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, poetry2nix }:
+  outputs = { self, poetry2nix }:
     let
-      system = "x86_64-linux"; # Modifique conforme sua arquitetura
-      pkgs = nixpkgs.legacyPackages.${system};
-      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
-      app = mkPoetryApplication { projectDir = ./.; };
+      system = "x86_64-linux";
     in
-    {
-      # apps.${system}.default = {
-      #   type = "app";
-      # };
-      # devShell.x86_64-linux = poetry2nix.mkPoetryApplication
-      #   {
-      #     src = ./.;
-      #   };
-      # devShell.x86_64-linux = pkgs.mkShell {
-      #   buildInputs = [
-      #     poetry2nix.packages.${pkgs.system}.poetry
-      #     poetry2nix.mkPoetryEnv
-      #     {
-      #       projectDir = ./.;
-      #     }
-      #   ];
-      # };
-      # defaultPackage.${system} = poetry2nix.mkPoetryApplication {
-      #   src = ./.;
-      # };
-    };
+    { };
 }
 
