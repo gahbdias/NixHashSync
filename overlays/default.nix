5b1a35b6db7039a98d7bd324bda444402ba8fd02
luis <ldesiqueira@pinger.com>
2024-10-14 14:36:51 -0300

    reduce flake.nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,6 @@
 [
   (self: super: let 
-    version = "1.8.2";
+    version = "1.8.3";
   in {
     super-poetry = super.poetry.overrideAttrs (oldAttrs: rec {
       inherit version;
@@ -9,8 +9,8 @@
         inherit version;
         owner = "python-poetry";
         repo = "poetry";
-        rev = "1.8.2";
-        sha256 = "058vyrby3q4632rgwfyix7fw0wjy51rqh7nmg3g9q7nl5xwra59h";
+        rev = "1.8.3";
+        sha256 = "0s0d55x9rjvndm22fd6ibl9bgxnhzv14p03vhvmrqp5rc7sfvw9w";
       };
     });
   })
